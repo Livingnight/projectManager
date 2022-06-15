8b0550dfd120cc9b1182001d53470296ef707d12
Jay Jernigan <jjernigan1065@gmail.com>
2022-06-15 14:50:11 -0400

    moving the database connection to coincide with the server connection

--- backend/index.js
+++ backend/index.js
@@ -8,9 +8,6 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-//connect to database
-db();
-
 app.use(
   "/graphql",
   graphqlHTTP({
@@ -20,5 +17,7 @@ app.use(
 );
 
 app.listen(PORT, () => {
+  //connect to database
+  db();
   console.log(`Server running on  port: ${PORT}`);
 });
